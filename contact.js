6731fb8437e03d49298ac2bbc8c4ef2a8d091eed
IrinaChoulgan <irina_choulgan@icloud.com>
2021-12-10 19:48:37 +0200

    +

--- contact.js
+++ contact.js
@@ -2,11 +2,8 @@ const fs = require('fs/promises');
 const path = require('path');
 const crypto = require('crypto');
 
-const contactsPath = path.resolve('./db/contacts.json');
-
 const readContent = async () => {
-    const content = await fs.readFile(contactsPath, 'utf8')
-    // const content = await fs.readFile(path.join(__dirname, 'db', 'contacts.json'), 'utf8')
+    const content = await fs.readFile(path.join(__dirname, 'db', 'contacts.json'), 'utf8')
     const result = JSON.parse(content)
     return result
 }
@@ -15,16 +12,31 @@ const listContacts = async () => {
     return await readContent()
   }
   
-  const getContactById = (contactId) => {
-    // ...твой код
+  const getContactById = async (contactId) => {
+    const contacts = await readContent();
+    const [result] = contacts.filter((contact) => contact.id === contactId)
+    return result
   }
   
-  const removeContact = (contactId) => {
-    // ...твой код
+  const removeContact = async (contactId) => {
+    const contacts = await readContent();
+    const result = contacts.filter((contact) => contact.id !== contactId)
+    await fs.writeFile(
+        path.join(__dirname, 'db', 'contacts.json'), 
+        JSON.stringify(result)
+        )
+    return result
   }
   
-  const addContact = (name, email, phone) => {
-    // ...твой код
+  const addContact = async (name, email, phone) => {
+    const contacts = await readContent();
+    const newContact = {name, email, phone, id: crypto.randomUUID()};
+    contacts.push(newContact);
+    await fs.writeFile(
+        path.join(__dirname, 'db', 'contacts.json'), 
+        JSON.stringify(contacts, null, 2)
+        )
+    return newContact
   }
   
   module.exports = {
